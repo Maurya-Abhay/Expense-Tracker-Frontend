89e3b6393045817e7b360df3177126f1877e85ae
Maurya-Abhay <167536919+Maurya-Abhay@users.noreply.github.com>
2024-11-28 15:04:08 +0530

    RefrshHandler.js

--- src/RefrshHandler.js
+++ src/RefrshHandler.js
@@ -1,25 +1,25 @@
 import React, { useEffect } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 
-function RefrshHandler({ setIsAuthenticated }) {
+function RefreshHandler({ setIsAuthenticated }) {
     const location = useLocation();
     const navigate = useNavigate();
 
     useEffect(() => {
         if (localStorage.getItem('token')) {
             setIsAuthenticated(true);
-            if (location.pathname === '/' ||
-                location.pathname === '/login' ||
-                location.pathname === '/signup'
-            ) {
-                navigate('/home', { replace: false });
+            
+            if (location.pathname === '/' || location.pathname === '/login' || location.pathname === '/signup') {
+                navigate('/home', { replace: true });
+            }
+        } else {
+            if (location.pathname !== '/login' && location.pathname !== '/signup') {
+                navigate('/login', { replace: true });
             }
         }
-    }, [location, navigate, setIsAuthenticated])
+    }, [location, navigate, setIsAuthenticated]);
 
-    return (
-        null
-    )
+    return null;
 }
 
-export default RefrshHandler
+export default RefreshHandler;
